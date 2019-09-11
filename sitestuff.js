3b078f784308f0363cbe4144740d671371463748
kash851 <kashyap.bk.rao@gmail.com>
2019-09-11 17:45:57 -0400

    Update sitestuff.js

--- sitestuff.js
+++ sitestuff.js
@@ -1,6 +1,7 @@
 window.onload = function() {
  var luckynumber = Math.Floor(Math.Random()*10);
- function roulette(var num) {
+ function roulette() {
+  var num = document.getElementById("input").value;
   if(num == luckynumber) {
     document.getElementById("sitetest").innerHTML = "Wow, epic!";
   } else {
