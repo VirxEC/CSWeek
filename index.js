ce4653375963f795cb3e06db9c4a3c35cf3b8d85
Virx <ericmveilleux@gmail.com>
2019-09-11 14:47:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-
+var c = document.querySelector(".console");
+console.log = (...args) => args.forEach(m => {
+  try {
+    c.appendChild(document.createTextNode(`\n ${m}`));
+  } catch(err) {
+    console.log(err);
+  }
+});
+console.warn = (...args) => args.forEach(e => {
+  try {
+    const s = document.createElement("span");
+      s.textContent = "\n" + e;
+      s.style.color = "rgb(205, 205, 0)";
+      c.appendChild(s);
+  } catch(err) {
+    console.warn(err);
+  }
+});
+console.error = (...args) => args.forEach(e => {
+  try {
+    const s = document.createElement("span");
+      s.textContent = "\n" + e;
+      s.style.color = "red";
+      c.appendChild(s);
+  } catch(err) {
+    console.error(err);
+  }
+});
+window.onerror=(e,s,l,c)=>console.error(`${e} at: ${s} : ${l}:${c}`);
