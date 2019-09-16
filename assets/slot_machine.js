5922ea26108129a0280464b885e629e36d1913ff
Virx <ericmveilleux@gmail.com>
2019-09-16 08:25:43 -0400

    Update slot_machine.js

--- assets/slot_machine.js
+++ assets/slot_machine.js
@@ -4,49 +4,28 @@ const SLOTS_PER_REEL = 12;
 const REEL_RADIUS = 150;
 
 function createSlots (ring) {
-	
 	var slotAngle = 360 / SLOTS_PER_REEL;
-
 	var seed = getSeed();
 
 	for (var i = 0; i < SLOTS_PER_REEL; i ++) {
 		var slot = document.createElement('div');
-		
 		slot.className = 'slot';
-
-		// compute and assign the transform for this slot
 		var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
-
 		slot.style.transform = transform;
-
-		// setup the number to show inside the slots
-		// the position is randomized to 
-
 		var content = $(slot).append('<p>' + ((seed + i)%12)+ '</p>');
-
-		// add the poster to the row
 		ring.append(slot);
 	}
 }
 
 function getSeed() {
-	// generate random number smaller than 13 then floor it to settle between 0 and 12 inclusive
 	return Math.floor(Math.random()*(SLOTS_PER_REEL));
 }
 
 function spin(timer) {
-	//var txt = 'seeds: ';
 	for(var i = 1; i < 6; i ++) {
 		var oldSeed = -1;
-		/*
-		checking that the old seed from the previous iteration is not the same as the current iteration;
-		if this happens then the reel will not spin at all
-		*/
 		var oldClass = $('#ring'+i).attr('class');
-		if(oldClass.length > 4) {
-			oldSeed = parseInt(oldClass.slice(10));
-			console.log(oldSeed);
-		}
+		if(oldClass.length > 4) oldSeed = parseInt(oldClass.slice(10));
 		var seed = getSeed();
 		while(oldSeed == seed) {
 			seed = getSeed();
@@ -56,40 +35,35 @@ function spin(timer) {
 			.css('animation','back-spin 1s, spin-' + seed + ' ' + (timer + i*0.5) + 's')
 			.attr('class','ring spin-' + seed);
 	}
-
-	console.log('=====');
 }
 
 $(document).ready(function() {
-
-	// initiate slots 
  	createSlots($('#ring1'));
  	createSlots($('#ring2'));
  	createSlots($('#ring3'));
  	createSlots($('#ring4'));
  	createSlots($('#ring5'));
-
- 	// hook start button
  	$('.go').on('click',function(){
- 		var timer = 2;
- 		spin(timer);
+		if (parseInt(localStorage.getItem("OwO")) > 0) {
+			localStorage.setItem("OwO", OwO-1);
+			var timer = 2;
+			spin(timer);
+		} else alert("Not enough OwO's!\n(Wins on Lucky Charms)");
  	})
 
- 	// hook xray checkbox
  	$('#xray').on('click',function(){
- 		//var isChecked = $('#xray:checked');
  		var tilt = 'tiltout';
  		
-    if($(this).is(':checked')) {
+    		if($(this).is(':checked')) {
  			tilt = 'tiltin';
  			$('.slot').addClass('backface-on');
  			$('#rotate').css('animation',tilt + ' 2s 1');
 
 			setTimeout(function(){
-			  $('#rotate').toggleClass('tilted');
+				$('#rotate').toggleClass('tilted');
 			},2000);
  		} else {
-      tilt = 'tiltout';
+      			tilt = 'tiltout';
  			$('#rotate').css({'animation':tilt + ' 2s 1'});
 
 			setTimeout(function(){
@@ -97,9 +71,8 @@ $(document).ready(function() {
 	 			$('.slot').removeClass('backface-on');
 	 		},1900);
  		}
- 	})
+ 	});
 
- 	// hook perspective
  	$('#perspective').on('click',function(){
  		$('#stage').toggleClass('perspective-on perspective-off');
  	})	
