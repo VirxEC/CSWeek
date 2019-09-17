0673848530733e058707b5c94349f726545b8644
Virx <ericmveilleux@gmail.com>
2019-09-17 14:00:04 -0400

    Update luckycharms.js

--- assets/luckycharms.js
+++ assets/luckycharms.js
@@ -1,64 +1,55 @@
-var OwO = localStorage.getItem("OwO"), UwU = localStorage.getItem("UwU"), bonus = (localStorage.getItem("bonus") == 'true'), cps = localStorage.getItem("cps");
-function setValue(id, value) {
-  eval(id+"="+value+";");
+var bonus = localStorage.getItem("bonus") == 'true';
+
+function setValue(id, value, returnV = false) {
   localStorage.setItem(id, value.toString());
+  if (returnV) return value;
+  else eval(id+"="+value+";");
 }
-if (OwO != null) {
-  OwO = parseInt(OwO);
-  if (OwO != 0) document.getElementById("OwO").textContent = "OwO ("+OwO+")";
-} else {
-  setValue("OwO", 0);
-}
-if (UwU != null) {
-  UwU = parseInt(UwU);
-  if (OwO != 0) document.getElementById("ratio").textContent = "| Luck: "+(OwO/UwU)*1000;
-  if (UwU != 0) document.getElementById("UwU").textContent = "UwU ("+UwU+")";
-} else {
-  setValue("UwU", 0);
-}
-if (cps != null) cps = parseInt(cps);
-else {
-  setValue("cps", 1);
+function setUpValue(value) {
+  eval(`var ${value}=localStorage.getItem("${value}");`);
+  eval(`${value}=${value}!=null ? +${value}:setValue("${value}", 0, true);`);
 }
 function startClicker() {
-  setInterval(()=>{
-    document.getElementById("roulettebruh").click();
-  }, 1000/cps);
+  setInterval(()=>document.getElementById("roulettebruh").click(), 1000/cps);
 }
+function setOwO(value) {
+  setValue("OwO", value);
+  document.getElementById("OwO").textContent = OwO == 0 ? "" : `OwO (${OwO})`;
+  document.getElementById("ratio").textContent = "| Luck: "+(OwO/UwU)*1000;
+}
+
+setUpValue("OwO");
+setUpValue("UwU");
+setUpValue("cps");
+if (UwU != 0) document.getElementById("UwU").textContent = "UwU ("+UwU+")";
+if (OwO != 0) setOwO(OwO);
+               
 if (localStorage.getItem("autoclicker") == 'true') startClicker();
 
 document.getElementById("roulettebruh").addEventListener("click", ()=>{
-  var rangen = Math.floor(Math.random()*1000)+1, num = parseInt(document.getElementById("input").value);
+  var rangen = ~~(Math.random()*1000)+1, num = +document.getElementById("input").value;
   if (num < 1001 && num > 0) {
     if(num == rangen || (bonus == true && (1000 - num) == rangen)) {
-      setValue("OwO", OwO+1);
+      setOwO(OwO+1);
       alert("You got lucky!\n+1 OwO!\n+"+(((OwO/UwU)*1000)-(((OwO-1)/UwU)*1000)).toString()+" luck!");
-      document.getElementById("OwO").textContent = "OwO ("+OwO+")";
     } else {
       setValue("UwU", UwU+1);
       document.getElementById("UwU").textContent = "UwU ("+UwU+")";
     }
-
     document.getElementById("ratio").textContent = "| Luck: "+(OwO/UwU)*1000;
-    Number.prototype.between = function(a, b, inclusive) {
-      var min = Math.min(a, b), max = Math.max(a, b);
-      return inclusive ? this >= min && this <= max : this > min && this < max;
-    }
 
     var onum = document.getElementById("number");
     onum.textContent = "| "+rangen;
-    if (rangen.between(num-10, num+10, true) || (bonus == true && rangen.between(990-num, 1010-num, true))) onum.style.color = "rgb(0, 255, 0)";
-    else if (rangen.between(num-100, num+100, true) || (bonus == true && rangen.between(900-num, 1100-num, true))) onum.style.color = "rgb(255, 255, 0)";
+    if (num-10 <= rangen && rangen <= num+10 || (bonus == true && 990-num <= rangen && rangen <= 1010-num)) onum.style.color = "rgb(0, 255, 0)";
+    else if (num-100 <= rangen && rangen <= num+100 || (bonus == true && 900-num <= rangen && rangen <= 1100-num)) onum.style.color = "rgb(255, 255, 0)";
     else onum.style.color = "rgb(255, 0, 0)";
   }
 });
 
 document.getElementById("bonus").addEventListener("click", ()=>{
   if (OwO >= 1 && bonus == false) {
-    setValue("OwO", OwO-1);
+    setOwO(OwO-1);
     localStorage.setItem("autoclicker", true);
-    if (OwO == 0) document.getElementById("OwO").textContent = "";
-    else document.getElementById("OwO").textContent = "OwO ("+OwO+" times)";
     setValue("bonus", true);
   } else if (bonus) alert("You already have this bonus!");
   else alert("Not enough OwO's!");
@@ -68,9 +59,9 @@ var autoclick = document.getElementById("autoclicker");
 if (localStorage.getItem("autoclicker") != 'true') {
   autoclick.addEventListener("click", ()=>{
     if (OwO > 99) {
-      setValue("OwO", OwO-100);
-      document.getElementById("OwO").textContent = `OwO (${OwO})`;
+      setOwO(OwO-100);
       localStorage.setItem("autoclicker", true);
+      setValue("cps", 1);
       startClicker();
       setClickPlus();
     } else if (OwO < 100) alert("Not enought OwO's!");
@@ -82,6 +73,7 @@ function setClickPlus() {
   autoclick.textContent = `Improve autoclicker for 100 OwO's (${cps} of 10 cps)`;
   document.getElementById("clickplus").addEventListener("click", ()=>{
     if (OwO > 99 && cps < 11) {
+      setOwO(OwO-100);
       setValue("cps", cps+1);
       autoclick.textContent = `Improve autoclicker for 100 OwO's (${cps} of 10 cps)`;
     } else if (OwO < 100) alert("Not enough OwO's!");
